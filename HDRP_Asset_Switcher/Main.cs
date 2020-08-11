33a007aa2f979a50af54ff804716a1e2a5488642
SqueegeeDinoToy <65366947+SqueegeeDino@users.noreply.github.com>
2020-08-11 14:59:20 -0700

    Created custom assetbundle, setting up to call from it
    Can currently break SXL with pink shaders, and can debug what asset bundle is currently active, now to switch successfully'

---
+++ HDRP_Asset_Switcher/Main.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+using Object = UnityEngine.Object;
+
+namespace HDRP_Asset_Switcher
+{
+    public class Main
+    {
+        public static bool Load(UnityModManager.ModEntry mod_entry)
+        {
+            mod_entry.OnToggle = OnToggle;
+
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry mod_entry, bool value)
+        {
+            if (value)
+            {
+                var go = new GameObject("SwitcherManager", typeof(SwitcherManager));
+
+
+                Object.DontDestroyOnLoad(go);
+            }
+            else
+            {
+
+                Debug.Log("TestMod - Value False");
+
+                var rm = Object.FindObjectOfType<SwitcherManager>();
+                if (rm != null)
+                    Object.Destroy(rm.gameObject);
+            }
+            return true;
+        }
+    }
+}
---
+++ HDRP_Asset_Switcher/SwitcherManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+using UnityEngine.Rendering;
+
+namespace HDRP_Asset_Switcher
+{
+
+    public class SwitcherManager : MonoBehaviour
+    {
+        private bool assetSwap;
+        public RenderPipelineAsset customHDRPAsset_1;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F10))
+            {
+                Debug.Log("[HDRP_Switcher] F10 key was pressed.");
+                {
+                    GraphicsSettings.renderPipelineAsset = customHDRPAsset_1;
+                    Debug.Log("[HDRP_Switcher] Active render pipeline asset is: " + GraphicsSettings.renderPipelineAsset.name);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.F11))
+            {
+                Debug.Log("[HDRP_Switcher] Active render pipeline asset is: " + GraphicsSettings.renderPipelineAsset.name);
+                Debug.Log("[HDRP_Switcher] F11 Key was pressed");
+            }
+        }
+        public static byte[] ExtractResource("HDRP_Asset_Switcher.HDRPAsset_SDT.asset")
+        {
+            Assembly a = Assembly.GetExecutingAssembly();
+            using (var resFilestream = a.GetManifestResourceStream("HDRP_Asset_Switcher.HDRPAsset_SDT.asset"))
+            {
+                if (resFilestream == null) return null;
+                byte[] ba = new byte[resFilestream.Length];
+                resFilestream.Read(ba, 0, ba.Length);
+                return ba;
+            }
+        }
+    }
+}
